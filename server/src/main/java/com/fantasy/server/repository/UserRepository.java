5e4db73f6f66246738205172921058f68b1ce4ab
Caleb Chung <kaichun24@gmail.com>
2024-09-16 21:36:05 -0700

    delete extra import

--- server/src/main/java/com/fantasy/server/repository/UserRepository.java
+++ server/src/main/java/com/fantasy/server/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.fantasy.server.repository;
 
-import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
