e610cda19d4f021165bf2af5bfa707e0e81c8783
Caleb C <kaichun24@gmail.com>
2025-04-02 10:28:41 -0700

    pom and autowired jwtservice bean

--- server/src/main/java/com/fantasy/server/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/fantasy/server/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.fantasy.server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,6 +24,7 @@ import lombok.NonNull;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
 
+    @Autowired
     private final JwtService jwtService;
 
     private final UserDetailsService userDetailsService;
