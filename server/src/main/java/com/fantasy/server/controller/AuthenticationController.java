eec18ce9190d8826db65c02e3b2f277e31ec8616
Caleb C <kaichun24@gmail.com>
2025-04-02 14:25:42 -0700

    rmeoved minor unused imports

--- server/src/main/java/com/fantasy/server/controller/AuthenticationController.java
+++ server/src/main/java/com/fantasy/server/controller/AuthenticationController.java
@@ -1,6 +1,5 @@
 package com.fantasy.server.controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- server/src/main/java/com/fantasy/server/service/AuthenticationService2.java
+++ server/src/main/java/com/fantasy/server/service/AuthenticationService2.java
@@ -1,6 +1,5 @@
 package com.fantasy.server.service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
